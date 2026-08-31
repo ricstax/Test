d53d4e4f6695a741fa82858043218a80ab687a70
Richard Junior Nehme <148864708+ricstax@users.noreply.github.com>
2026-08-31 19:43:45 +0300

    Fix Android Kotlin and Compose build plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
 plugins {
     id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-}
-
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlin.plugin.compose:org.jetbrains.kotlin.plugin.compose.gradle.plugin:2.3.21")
-    }
 }
